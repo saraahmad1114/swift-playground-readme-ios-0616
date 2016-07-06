95306a96b3be4d514be1ca4859eeb6443d3084c1
Sara <saraahmad1114@yahoo.com>
2016-07-06 09:25:42 -0400

    constants and variables

---
+++ SecondLab.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+str = "Hello, there!"
+//When you declare variables with var, they are mutable but if you use let then they are not mutable similarly with NSString vs. NSMutableString 
+
